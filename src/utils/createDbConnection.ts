a931ca1ef68b2c3483887a42c865076ba6836160
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2022-11-14 23:52:50 +0200

    add db connection init

---
+++ src/utils/createDbConnection.ts
@@ -0,0 +1,37 @@
+import mariadb from 'mariadb';
+import { Sequelize } from 'sequelize-typescript';
+
+export default async function createDbConnection(host: string, port: number, username: string, password: string, database: string) {
+    await createDatabaseIfNotExists(host, port, username, password, database);
+
+    const connection = new Sequelize({
+        dialect: "mariadb",
+        host: host,
+        port: port,
+        username: username,
+        password: password,
+        database: database,
+        logging: false,
+        models: [__dirname + '/../entities/*.entity.ts'],
+        });
+        
+    await connection.sync();
+
+    return connection;
+}
+
+async function createDatabaseIfNotExists(host: string, port: number, username: string, password: string, database: string) {
+    const pool = mariadb.createPool({
+        host: host,
+        port: port,
+        user: username,
+        password: password,
+        connectionLimit: 5
+    });
+
+    const conn = await pool.getConnection();
+
+    await conn.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+    conn.release();
+}
