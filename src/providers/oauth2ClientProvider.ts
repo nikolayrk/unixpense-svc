e20b6d6174b3599154c5abefd36c8b88abbbffcd
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-03-12 04:37:27 +0200

    persist refresh token on all authorisations

--- src/providers/oauth2ClientProvider.ts
+++ src/providers/oauth2ClientProvider.ts
@@ -20,7 +20,8 @@ export default class OAuth2ClientProvider {
         this._oauth2Client = value;
     }
 
-    private authenticated;
+    private authenticated: boolean;
+    private refreshToken?: string;
 
     constructor(clientId: string, clientSecret: string, redirectUri: string, refreshTokenRepository: RefreshTokenRepository) {
         const oauth2Client = new google.auth.OAuth2(
@@ -33,7 +34,51 @@ export default class OAuth2ClientProvider {
         this._oauth2Client = oauth2Client;
         this.authenticated = false;
 
-        oauth2Client.on('tokens', this.receivedTokensAsync);
+        oauth2Client.on('tokens', async (tokens) => {
+            const receivedRefreshToken = tokens.refresh_token !== null && tokens.refresh_token !== undefined
+                ? tokens.refresh_token
+                : null;
+
+            if (receivedRefreshToken !== null) {
+                console.log(`Persisting refresh token received via 'tokens' event: ${JSON.stringify(tokens)}`);
+
+                await this.refreshTokenRepository.createIfNotExistAsync(receivedRefreshToken);
+
+                this.setRefreshToken(receivedRefreshToken);
+            }
+
+            const storedRefreshToken = this.refreshToken !== undefined
+                ? this.refreshToken
+                : null;
+
+            const persistedRefreshToken = storedRefreshToken === null
+                ? await this.refreshTokenRepository.getRefreshTokenOrNullAsync()
+                : null;
+
+            if (persistedRefreshToken !== null) {
+                console.log(`Using persisted refresh token from 'tokens' event: ${persistedRefreshToken}`);
+
+                this.setRefreshToken(persistedRefreshToken);
+            }
+                
+            const finalRefreshToken = receivedRefreshToken !== null
+                ? receivedRefreshToken
+                : storedRefreshToken !== null
+                    ? storedRefreshToken
+                    : persistedRefreshToken;
+
+            if (finalRefreshToken === null) {
+                // No refresh token from the tokens event, nor previously stored, nor persisted
+                // Should not happen
+                console.log(`No refresh token from the tokens event, nor previously received, nor persisted`)
+
+                return;
+            }
+
+            console.log(`Using credentials received via 'tokens' event: ${JSON.stringify(tokens)}`);
+
+            this.authenticate(tokens);
+        });
     }
 
     public async checkAuthenticatedAsync() {
@@ -44,12 +89,16 @@ export default class OAuth2ClientProvider {
         const refreshToken = await this.refreshTokenRepository.getRefreshTokenOrNullAsync();
 
         if (refreshToken === null) {
+            console.log(`Couldn't find persisted refresh token`);
+
             return false;
         }
 
-        this.authenticate({
-            refresh_token: refreshToken
-        });
+        console.log(`Using persisted refresh token: ${refreshToken}`);
+
+        this.setRefreshToken(refreshToken);
+
+        this.authenticate();
 
         return true;
     }
@@ -57,6 +106,8 @@ export default class OAuth2ClientProvider {
     public async authenticateWithCodeAsync(authorizationCode: string) {
         const tokenResponse = await this.oauth2Client.getToken(authorizationCode);
         const accessToken = tokenResponse.tokens;
+
+        console.log(`Using access token received via code: ${JSON.stringify(accessToken)}`);
 
         this.authenticate(accessToken);
     }
@@ -70,19 +121,25 @@ export default class OAuth2ClientProvider {
         return url;
     }
 
-    private async receivedTokensAsync(tokens: Credentials) {
-        const refreshToken = tokens.refresh_token;
+    private authenticate(credentials: Credentials = {}) {
+        const {refresh_token, ...rest} = credentials;
+        const passedRefreshToken = refresh_token;
+        
+        const refreshToken = passedRefreshToken !== null && passedRefreshToken !== undefined
+            ? passedRefreshToken
+            : this.refreshToken;
 
-        if (refreshToken !== null && refreshToken !== undefined) {
-            await this.refreshTokenRepository.createAsync(refreshToken);                
-        }
+        const fullCredentials: Credentials = {
+            refresh_token: refreshToken,
+            ...rest
+        };
 
-        this.authenticate(tokens);
-    }
-
-    private authenticate(credentials: Credentials) {
-        this.oauth2Client.setCredentials(credentials);
+        this.oauth2Client.setCredentials(fullCredentials);
 
         this.authenticated = true;
+    }
+
+    private setRefreshToken(refreshToken: string) {
+        this.refreshToken = refreshToken;
     }
 }
