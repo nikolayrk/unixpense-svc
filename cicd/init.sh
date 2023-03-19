e8e442be200210fdbb0763acf3b45cf9437604da
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-03-19 14:27:35 +0200

    cleaned up env var passing

--- cicd/init.sh
+++ cicd/init.sh
@@ -13,12 +13,13 @@ DOCKERCONFIGJSON=$(cat <<EOF
 EOF
 )
 
-kubectl create namespace ${UNIXPENSE_K8S_NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace ${UNIXPENSE_K8S_NAMESPACE} \
+    --dry-run=client -o yaml | kubectl apply -f -
 
 kubectl create configmap unixpense-svc-config \
     --dry-run=client -o yaml \
     --namespace=${UNIXPENSE_K8S_NAMESPACE} \
-    --from-literal=PORT=${UNIXPENSE_PORT} | kubectl apply -f -
+    --from-literal=UNIXPENSE_PORT=${UNIXPENSE_PORT} | kubectl apply -f -
 
 kubectl create secret generic unixpense-svc-regcred \
     --dry-run=client -o yaml \
@@ -29,15 +30,15 @@ kubectl create secret generic unixpense-svc-regcred \
 kubectl create secret generic unixpense-svc-googlecred \
     --dry-run=client -o yaml \
     --namespace=${UNIXPENSE_K8S_NAMESPACE} \
-    --from-literal=CLIENT_ID=${UNIXPENSE_GOOGLE_CLIENT_ID} \
-    --from-literal=CLIENT_SECRET=${UNIXPENSE_GOOGLE_CLIENT_SECRET} \
-    --from-literal=REDIRECT_URI=${UNIXPENSE_GOOGLE_REDIRECT_URI} | kubectl apply -f -
+    --from-literal=UNIXPENSE_GOOGLE_CLIENT_ID=${UNIXPENSE_GOOGLE_CLIENT_ID} \
+    --from-literal=UNIXPENSE_GOOGLE_CLIENT_SECRET=${UNIXPENSE_GOOGLE_CLIENT_SECRET} \
+    --from-literal=UNIXPENSE_GOOGLE_REDIRECT_URI=${UNIXPENSE_GOOGLE_REDIRECT_URI} | kubectl apply -f -
 
 kubectl create secret generic unixpense-svc-dbcred \
     --dry-run=client -o yaml \
     --namespace=${UNIXPENSE_K8S_NAMESPACE} \
-    --from-literal=DB_HOST=${UNIXPENSE_MARIADB_HOST} \
-    --from-literal=DB_PORT=${UNIXPENSE_MARIADB_PORT} \
-    --from-literal=DB_USERNAME=${UNIXPENSE_MARIADB_USERNAME} \
-    --from-literal=DB_PASSWORD=${UNIXPENSE_MARIADB_PASSWORD} \
-    --from-literal=DB_NAME=${UNIXPENSE_MARIADB_DATABASE} | kubectl apply -f -
+    --from-literal=UNIXPENSE_MARIADB_HOST=${UNIXPENSE_MARIADB_HOST} \
+    --from-literal=UNIXPENSE_MARIADB_PORT=${UNIXPENSE_MARIADB_PORT} \
+    --from-literal=UNIXPENSE_MARIADB_USERNAME=${UNIXPENSE_MARIADB_USERNAME} \
+    --from-literal=UNIXPENSE_MARIADB_PASSWORD=${UNIXPENSE_MARIADB_PASSWORD} \
+    --from-literal=UNIXPENSE_MARIADB_DATABASE=${UNIXPENSE_MARIADB_DATABASE} | kubectl apply -f -
