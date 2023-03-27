a80813b3435aea8d7680ab7738a3c88ec0342a9c
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-03-27 22:37:00 +0300

    add ts-jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // A list of paths to modules that run some code to configure or set up the testing framework before each test
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+};
