f9eef11f4888bb89ff86e0a63c02bec4a6daacee
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-04-15 23:20:26 +0300

    set NODE_ENV in more apt place

--- cicd/build.sh
+++ cicd/build.sh
@@ -3,4 +3,5 @@
 /kaniko/executor --context=dir://${WORKSPACE}/ \
                 --dockerfile=./cicd/Dockerfile \
                 --destination=${UNIXPENSE_DOCKER_REPO}:${GIT_COMMIT} \
-                --build-arg="PORT=${UNIXPENSE_PORT}"
+                --build-arg="PORT=${UNIXPENSE_PORT}" \
+                --build-arg="NODE_ENV=${NODE_ENV}"
