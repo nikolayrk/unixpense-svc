740163b6a82e4fc9f66f5ce6d87640681e462dd6
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-04-16 00:51:14 +0300

    pass node env & log lvl env via jenkins job

--- cicd/init.sh
+++ cicd/init.sh
@@ -19,6 +19,8 @@ kubectl create namespace ${UNIXPENSE_K8S_NAMESPACE} \
 kubectl create configmap unixpense-svc-config \
     --dry-run=client -o yaml \
     --namespace=${UNIXPENSE_K8S_NAMESPACE} \
+    --from-literal=NODE_ENV=${NODE_ENV} \
+    --from-literal=LOG_LEVEL=${LOG_LEVEL} \
     --from-literal=UNIXPENSE_PORT=${UNIXPENSE_PORT} \
     --from-literal=UNIXPENSE_LOKI_HOST=${UNIXPENSE_LOKI_HOST} | kubectl apply -f -
 
