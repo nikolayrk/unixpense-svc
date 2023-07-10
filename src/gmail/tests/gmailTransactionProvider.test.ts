e85432d7cd7584599ba17612a04b0d5504bda7fe
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-07-11 02:33:03 +0300

    reworked unit tests

---
+++ src/gmail/tests/gmailTransactionProvider.test.ts
@@ -0,0 +1,48 @@
+import { describe, it, beforeAll, expect } from '@jest/globals';
+import { DependencyInjector } from '../../dependencyInjector';
+import { injectables } from '../../core/types/injectables';
+import { PaymentDetailsTestCase, paymentDetailsTestCases } from '../types/paymentDetailsTestCases';
+import { registerDependencies } from '../../bootstrap';
+import PaymentDetails from '../../core/models/paymentDetails';
+import ITransactionProvider from '../../core/contracts/ITransactionProvider';
+import TransactionFactory from '../../core/factories/transactionFactory';
+import { transactionDataTestCase } from '../types/transactionDataTestCase';
+import { TransactionData } from '../../core/models/transactionData';
+import GoogleOAuth2IdentifiersFactory from '../../googleOAuth2/factories/googleOAuth2IdentifiersFactory';
+
+describe('Gmail Transaction Provider Tests', () => {
+    let transactionFactory: TransactionFactory;
+    let transactionProvider: ITransactionProvider;
+
+    beforeAll(async () => {
+        registerDependencies();
+
+        const googleOAuth2IdentifierFactory = DependencyInjector.Singleton.resolve<GoogleOAuth2IdentifiersFactory>(injectables.GoogleOAuth2IdentifiersFactory);
+        const oauth2Identifiers = googleOAuth2IdentifierFactory.create({});
+
+        transactionFactory = DependencyInjector.Singleton.resolve<TransactionFactory>(injectables.TransactionFactory);
+        transactionProvider = await DependencyInjector.Singleton.generateGmailServiceAsync<ITransactionProvider>(injectables.GmailTransactionProviderGenerator, oauth2Identifiers);
+    });
+
+    const describePaymentDetailsTest = (entry: [string, PaymentDetailsTestCase<PaymentDetails>]) => {
+        const transactionId = entry[0];
+        const testCase = entry[1];
+
+        it(`should resolve transaction of type ${transactionId}`, async () => {
+            const expectedTransactionData: TransactionData = {
+                ...transactionDataTestCase.expectedTransactionDataHead,
+                ...testCase.expectedTransactionDataBody
+            };
+            const expectedPaymentDetails = testCase.expectedPaymentDetails;
+            const expected = transactionFactory.create(transactionId, expectedTransactionData, expectedPaymentDetails);
+
+            const actual = await transactionProvider.resolveTransactionOrNullAsync(transactionId);
+
+            expect(actual).toEqual(expected);
+        });
+    }
+
+    Object
+        .entries(paymentDetailsTestCases)
+        .map(describePaymentDetailsTest);
+});
