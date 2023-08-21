91fccd942335c28ec8c7dfa582fc4cd5418d1b73
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2023-08-21 23:31:45 +0300

    fix slow running of tests

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   testEnvironment: 'node',
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+        isolatedModules: true
+      }],
+  },
 };
