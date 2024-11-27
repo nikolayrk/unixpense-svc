fc47d9fa6d88c7bcb1acfd894f21260a227175bb
nikolayrk <96291207+nikolayrk@users.noreply.github.com>
2024-11-27 02:54:33 +0200

    remove debug artefacts

--- sh/report_integration_tests_job_status.sh
+++ sh/report_integration_tests_job_status.sh
@@ -1,7 +1,7 @@
 JOB_NAME=$(kubectl get jobs --all-namespaces -l integration_tests=true -o jsonpath='{.items[0].metadata.name}')
 NAMESPACE=$(kubectl get jobs --all-namespaces -l integration_tests=true -o jsonpath='{.items[0].metadata.namespace}')
 
-echo Resolved job name: $JOB_NAME and namespace: $NAMESPACE
+echo Runngi job $JOB_NAME in namespace $NAMESPACE ...
 
 if kubectl wait --namespace $NAMESPACE --for=condition=complete --timeout=10s job/$JOB_NAME; then
     echo "Job completed successfully"
@@ -10,7 +10,6 @@ else
     
     # Get the pod for this job
     pod=$(kubectl get pods --namespace $NAMESPACE --selector=job-name=$JOB_NAME --output=jsonpath='{.items[*].metadata.name}')
-    app_pod=$(kubectl get pods --namespace $NAMESPACE --selector=debug_app=true --output=jsonpath='{.items[*].metadata.name}')
     
     if [ -n "$pod" ]; then
         echo "=== Pod Status ==="
@@ -21,13 +20,6 @@ else
         kubectl logs --namespace $NAMESPACE $pod --previous 2>/dev/null || echo "No previous logs found"
         kubectl logs --namespace $NAMESPACE $pod 2>/dev/null || echo "No current logs found"
         echo "=== End Pod Logs ==="
-        
-        echo "=== App Pod Logs ==="
-        kubectl get pods --namespace $NAMESPACE --selector=debug_app=true
-        kubectl logs -l debug_app=true --namespace $NAMESPACE
-        kubectl describe pod $app_pod -n $NAMESPACE
-        kubectl get events --field-selector involvedObject.name=$app_pod -n $NAMESPACE
-        echo "=== End App Pod Logs ==="
         
         echo "=== Job Status ==="
         kubectl describe job --namespace $NAMESPACE $JOB_NAME
